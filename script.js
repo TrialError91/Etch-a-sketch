0b721aee34b243cdd5163676aeb10fb15c2db979
Nicolò De Salvo <125812842+TrialError91@users.noreply.github.com>
2023-03-13 17:56:21 +0100

    Change the color of squares on mouse over

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ container.setAttribute("style", `display: grid; grid-template-columns:repeat(${d
 for (let i=0; i<(dimensions*dimensions); i++) {
     container.appendChild(document.createElement('div'))
 };
+
+let squares = document.querySelectorAll('.container div')
+let red
+let green
+let blue
+function setColor (e) {
+    red=+Math.floor(Math.random()*255)
+    green=+Math.floor(Math.random()*255)
+    blue=+Math.floor(Math.random()*255)
+    e.target.setAttribute("style", `background-color: rgb(${red}, ${green}, ${blue})`)
+};
+
+squares.forEach((square)=> {
+    square.addEventListener('mouseover', setColor, true)
+});
