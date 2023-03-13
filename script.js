4da977258ed41a9fcb2441f305db4cb7ea1b358f
Nicolò De Salvo <125812842+TrialError91@users.noreply.github.com>
2023-03-13 17:57:38 +0100

    Declare a function that sets the grid base on user input

--- script.js
+++ script.js
@@ -23,3 +23,29 @@ function setColor (e) {
 squares.forEach((square)=> {
     square.addEventListener('mouseover', setColor, true)
 });
+
+function setGrid () {
+    userInput=+prompt("Enter the number of rows and columns");
+    console.log(typeof userInput)
+    for (let i=0; i<(dimensions*dimensions); i++){
+        let div = document.querySelector('.container div')
+        container.removeChild(div)
+    };
+    if (userInput<4) {
+        dimensions=4
+    } else if (userInput>100) {
+        dimensions=100
+    } else {
+        dimensions=userInput
+    };
+    operator=(100/dimensions)
+    container.setAttribute("style", `display: grid; grid-template-columns:repeat(${dimensions}, ${operator}%);
+    grid-template-rows:repeat(${dimensions}, ${operator}%)`)
+    for (let i=0; i<(dimensions*dimensions); i++) {
+        container.appendChild(document.createElement('div'))
+    };
+    squares = document.querySelectorAll('.container div')
+    squares.forEach((square)=> {
+        square.addEventListener('mouseover', setColor, true)
+    });
+};
